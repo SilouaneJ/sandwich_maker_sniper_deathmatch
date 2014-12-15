791ea84dd196046f7a194f98f02dfb0123b361e6
Silouane Jeanneteau <silouane.jeanneteau@fishingcactus.com>
2014-12-15 16:52:23 +0100

    Cleaned sniper part

--- Assets/Scripts/SniperManager.cs
+++ Assets/Scripts/SniperManager.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class SniperManager : MonoBehaviour
 {
-	public GameObject CrossHairPrefab;
-	private GameObject CrossHair;
+	public GameObject CrossHair;
 
 	// Use this for initialization
 	void Start ()
@@ -15,6 +14,10 @@ public class SniperManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		Vector3
+			current_position;
+		current_position = CrossHair.transform.position;
+		current_position.x += Time.deltaTime * 0.1f;
+		CrossHair.transform.position = current_position;
 	}
 }
