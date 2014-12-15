bf65fea5a2db9abb997c295d3b3847d785d9188c
Silouane Jeanneteau <silouane.jeanneteau@fishingcactus.com>
2014-12-15 16:38:02 +0100

    Add sniper manager

---
+++ Assets/SniperManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SniperManager : MonoBehaviour
+{
+	public GameObject CrossHairPrefab;
+	private GameObject CrossHair;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
