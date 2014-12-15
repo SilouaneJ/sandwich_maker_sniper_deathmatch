4ee5b8dd01f4fd37195203ff5f8d1b105f9ad8b7
Silouane Jeanneteau <silouane.jeanneteau@fishingcactus.com>
2014-12-15 12:26:04 +0100

    Add first assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject SandwichMakerBoothPrefab;
+	private GameObject[] SandwichBoothTable;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SandwichBoothTable = new GameObject[2];
+		CreateScene ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void CreateScene()
+	{
+		SandwichBoothTable[0] = ((GameObject)Instantiate(SandwichMakerBoothPrefab));
+		SandwichBoothTable[1] = ((GameObject)Instantiate(SandwichMakerBoothPrefab));
+
+		InitializeBooth (0, new Vector3 (-1.3f, 0.4f, 0.0f));
+		InitializeBooth (1, new Vector3 (1.3f, 0.4f, 0.0f));
+	}
+
+	void InitializeBooth(uint booth_index, Vector3 position)
+	{
+		SandwichBoothTable [booth_index].transform.position = position;
+	}
+}
