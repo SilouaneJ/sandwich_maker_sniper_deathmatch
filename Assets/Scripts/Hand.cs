4a385809682b9d806a219b56494660228bfe26fc
U-Awesome1\Ferry <ferry.keesom@fishingcactus.com>
2014-12-16 14:39:11 +0100

    First itteration on grabbing an d dropping toppings

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+	[SerializeField]
+	private MeshFilter Surface;
+
+	private Vector3 SurfaceExtents;
+	private Vector3 SurfaceCenter;
+
+	private GameObject Topping;
+
+	// Use this for initialization
+	void Start () {
+		SurfaceExtents = Surface.mesh.bounds.extents;
+		SurfaceCenter = Surface.mesh.bounds.center;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 HalfViewportExtent;
+		Vector3 MousePosition;
+		Vector3 Position;
+
+		HalfViewportExtent = new Vector3( Screen.width / 2 , Screen.height / 2, 0 );
+
+		MousePosition = Input.mousePosition - HalfViewportExtent;
+		MousePosition.x /= HalfViewportExtent.x;
+		MousePosition.y /= HalfViewportExtent.y;
+
+		Position = transform.position;
+		Position.x = MousePosition.x * SurfaceExtents.x + SurfaceCenter.x;
+		Position.z = MousePosition.y * SurfaceExtents.y + SurfaceCenter.z;
+		transform.position = Position;
+
+		if ( HasTopping() && Input.GetMouseButtonDown( 0 ) && Topping )
+		{
+			DropTopping();
+		}
+	}
+
+	public void GrabTopping(GameObject prefab)
+	{
+		Topping = (GameObject) GameObject.Instantiate( prefab );
+		Topping.rigidbody.useGravity = false;
+		Topping.rigidbody.isKinematic = true;
+		Topping.transform.parent = transform;
+		Topping.transform.localPosition = Vector3.zero;
+	}
+
+	void DropTopping()
+	{
+		Topping.transform.parent = null;
+		Topping.rigidbody.isKinematic = false;
+		Topping.rigidbody.useGravity = true;
+		Topping = null;
+	}
+
+	public bool HasTopping()
+	{
+		return Topping != null;
+	}
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject ToppingPrefab;
+
+	void OnTriggerStay(Collider other)
+	{
+		Hand hand;
+		hand = other.gameObject.GetComponent<Hand>();
+
+		if ( hand != null && !hand.HasTopping() )
+		{
+			Bounds this_bounds = collider.bounds;
+			Bounds hand_bounds = hand.collider.bounds;
+
+			if( this_bounds.Contains( hand_bounds.center ) &&
+			   this_bounds.min.x < hand_bounds.min.x &&
+			   this_bounds.min.y < hand_bounds.min.y &&
+			   this_bounds.min.z < hand_bounds.min.z &&
+			   this_bounds.max.x > hand_bounds.max.x &&
+			   this_bounds.max.y > hand_bounds.max.y &&
+			   this_bounds.max.z > hand_bounds.max.z &&
+			   ToppingPrefab != null
+		    )
+			{
+				hand.GrabTopping( ToppingPrefab );
+			}
+		}
+	}
+}
