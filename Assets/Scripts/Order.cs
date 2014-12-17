3b7968f2462fa5950c6edffe1845967c35f21dc6
U-Awesome1\Ferry <ferry.keesom@fishingcactus.com>
2014-12-17 11:27:44 +0100

    Added Topping and Order scripts

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Order : MonoBehaviour 
+{
+	private List<Topping> ToppingTable;
+
+	// Use this for initialization
+	void Start () {
+		ToppingTable = new List<Topping>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Topping topping = collider.gameObject.GetComponent<Topping>();
+
+		if( topping != null )
+		{
+			ToppingTable.Add( topping );
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		Topping topping = collider.gameObject.GetComponent<Topping>();
+
+		if( topping != null )
+		{
+			ToppingTable.Remove( topping );
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+	}
+
+}
---
+++ Assets/Scripts/Topping.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ToppingType
+{
+	TOMATO,
+	LETTUCE,
+	CHEESE,
+	BUN
+}
+
+public class Topping : MonoBehaviour {
+
+	[SerializeField]
+	private ToppingType _Type;
+
+	public ToppingType Type
+	{ 
+		get { return _Type; }		
+	}
+}
+
