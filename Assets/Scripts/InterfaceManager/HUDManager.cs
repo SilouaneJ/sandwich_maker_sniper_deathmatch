b9dddf1e2764d4700736bf936965a11787c3eef0
Guillaume Bouckaert <bouckaert.g@gmail.com>
2014-12-18 15:21:52 +0100

    added client getting angry over time

---
+++ Assets/Scripts/InterfaceManager/HUDManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour {
+
+	[ SerializeField ] GameObject
+		HUDObjectPrefab;
+
+	GameObject
+		HUDObject;
+
+	[ SerializeField ] GameObject
+		LeftOrderPrefab,
+		RightOrderPrefab;
+
+	GameObject
+		LeftOrder,
+		RightOrder;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetUpHUDManager(){
+
+		ResetHUDObject();
+	}
+
+	public void ResetHUDObject(){
+
+		GameObject hud_object = HUDObject;
+		HUDObject = null;
+		Destroy(hud_object);
+	}
+
+	public void StartHUDManager(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateHUDManager(){
+
+
+	}
+}
---
+++ Assets/Scripts/InterfaceManager/InterfaceManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour {
+
+	[ SerializeField ] GameObject
+		MainManager,
+		HUDManager,
+		ScoreManager;
+
+	bool
+		InMain,
+		InHUD,
+		InScore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetUpInterfaceManager(){
+
+		InMain = false;
+		InHUD = false;
+		InScore = false;
+
+		HUDManager.GetComponent<HUDManager>().SetUpHUDManager();
+	}
+
+	public void LaunchHUD(){
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateInterfaceManager(){
+
+		if (InHUD){
+
+			HUDManager.GetComponent<HUDManager>().UpdateHUDManager();
+		}
+	}
+}
